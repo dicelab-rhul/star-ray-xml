aa639a0ef09818c3cf5b10274d4cc95160940050
Benedict <brjw@hotmail.co.uk>
2024-07-17 15:46:08 +0100

    updated readme

--- setup.py
+++ setup.py
@@ -1,17 +1,19 @@
+"""Setup file."""
+
 from setuptools import setup, find_packages
 
 setup(
     name="star_ray_xml",
-    version="0.0.2",
+    version="0.0.3",
     author="Benedict Wilkins",
     author_email="benrjw@gmail.com",
-    description="An optional extra for the `star_ray` package that supports XML environment state and associated query events.",
+    description="An extension to the [star_ray](https://github.com/dicelab-rhul/star-ray) platform that uses XML as a description language for the state of an environment. Agents may use [xpath](https://www.w3schools.com/xml/xpath_intro.asp) expressions to query (read/write) to the state.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     packages=find_packages(),
-    url="https://github.com/dicelab-rhul/star_ray_xml",
+    url="https://github.com/dicelab-rhul/star-ray-xml",
     install_requires=[
-        "star_ray>=0.0.3",
+        "star_ray>=0.0.4",
         "lxml",
         "pydantic",
     ],
