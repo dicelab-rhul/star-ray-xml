4d5074ef403978b6f41ac271f0a393e71c3f7de1
Benedict <brjw@hotmail.co.uk>
2024-07-30 07:34:44 +0100

    minor

--- star_ray_xml/state.py
+++ star_ray_xml/state.py
@@ -214,7 +214,6 @@ class _XMLState(XMLState):
         """
         elements = self.xpath(query.xpath)
         result = [_XMLState.select_from_element(element, query) for element in elements]
-        # print(result)
         return result
 
     @staticmethod
